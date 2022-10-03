bf1a9c063ddaaa387263a71b6a570a010288e502
MorganaLF <viktoriya.chernenko@metalamp.io>
2022-10-03 18:32:33 +0300

    Remove next, move docs into root folder

--- src/docs/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LEGAL_NOTICE_URL } from './Apostro Legal Notice 2022-09-30.pdf';
-export { default as PRIVACY_NOTICE_URL } from './Apostro Privacy Notice 2022-09-30.pdf';
--- src/routes/routes.ts
+++
@@ -1,5 +0,0 @@
-import build from 'build-route-tree';
-
-const rawTree = {};
-
-export const routes = build(rawTree);
